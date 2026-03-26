33755b7502c8e7ff5e7c4b9a00656b5fc0c93d1b
amal-prasad <amalprasad1998@gmail.com>
2026-03-26 17:52:18 +0530

    Revert config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let repoPath = '';
-if (isGithubActions && process.env.GITHUB_REPOSITORY) {
-  const repoName = process.env.GITHUB_REPOSITORY.split('/')[1];
-  repoPath = `/${repoName}`;
-}
-
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: repoPath,
+  /* config options here */
 };
 
 export default nextConfig;
